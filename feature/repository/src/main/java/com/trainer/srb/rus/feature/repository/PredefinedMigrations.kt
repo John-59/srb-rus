d3d8b24c7b875c68ad200e84d4298e851497fc7c
Evgenii Zaichkin <evgeniy.v.zaichkin@gmail.com>
2023-12-29 17:35:33 +0100

    Add some words, migrate predefined repo to version 10.

--- feature/repository/src/main/java/com/trainer/srb/rus/feature/repository/PredefinedMigrations.kt
+++ feature/repository/src/main/java/com/trainer/srb/rus/feature/repository/PredefinedMigrations.kt
@@ -13,19 +13,31 @@ fun <T: RoomDatabase> RoomDatabase.Builder<T>.applyPredefinedMigrations(): RoomD
         .addMigrations(MIGRATION_ASSETS_5_6)
         .addMigrations(MIGRATION_ASSETS_6_7)
         .addMigrations(MIGRATION_ASSETS_7_8)
+        .addMigrations(MIGRATION_ASSETS_8_9)
+        .addMigrations(MIGRATION_ASSETS_9_10)
 }
 
-private val MIGRATION_ASSETS_1_2 = object : Migration(1, 2) {
+private val MIGRATION_ASSETS_9_10 = object : Migration(9, 10) {
     override fun migrate(db: SupportSQLiteDatabase) {
     }
 }
 
-private val MIGRATION_ASSETS_2_3 = object : Migration(2, 3) {
+private val MIGRATION_ASSETS_8_9 = object : Migration(8, 9) {
     override fun migrate(db: SupportSQLiteDatabase) {
     }
 }
 
-private val MIGRATION_ASSETS_3_4 = object : Migration(3, 4) {
+private val MIGRATION_ASSETS_7_8 = object : Migration(7, 8) {
+    override fun migrate(db: SupportSQLiteDatabase) {
+    }
+}
+
+private val MIGRATION_ASSETS_6_7 = object : Migration(6, 7) {
+    override fun migrate(db: SupportSQLiteDatabase) {
+    }
+}
+
+private val MIGRATION_ASSETS_5_6 = object : Migration(5, 6) {
     override fun migrate(db: SupportSQLiteDatabase) {
     }
 }
@@ -36,17 +48,22 @@ private val MIGRATION_ASSETS_4_5 = object : Migration(4, 5) {
     }
 }
 
-private val MIGRATION_ASSETS_5_6 = object : Migration(5, 6) {
+private val MIGRATION_ASSETS_3_4 = object : Migration(3, 4) {
     override fun migrate(db: SupportSQLiteDatabase) {
     }
 }
 
-private val MIGRATION_ASSETS_6_7 = object : Migration(6, 7) {
+private val MIGRATION_ASSETS_2_3 = object : Migration(2, 3) {
     override fun migrate(db: SupportSQLiteDatabase) {
     }
 }
 
-private val MIGRATION_ASSETS_7_8 = object : Migration(7, 8) {
+private val MIGRATION_ASSETS_1_2 = object : Migration(1, 2) {
     override fun migrate(db: SupportSQLiteDatabase) {
     }
 }
+
+
+
+
+
--- feature/repository/src/main/java/com/trainer/srb/rus/feature/repository/PredefinedRepositoryDatabase.kt
+++ feature/repository/src/main/java/com/trainer/srb/rus/feature/repository/PredefinedRepositoryDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 
 @Database(
-    version = 8,
+    version = 10,
     exportSchema = true,
     entities = [
         SerbianLatinWord::class,
