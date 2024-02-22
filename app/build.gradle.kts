4072564a4b9269e06b6202402ff0e6443b7146e5
Evgenii Zaichkin <evgeniy.v.zaichkin@gmail.com>
2024-02-22 23:25:53 +0100

    Version 0.6.20

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.trainer.srb.rus"
         minSdk = 24
         targetSdk = 34
-        versionCode = 520
-        versionName = "0.5.20"
+        versionCode = 620
+        versionName = "0.6.20"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
