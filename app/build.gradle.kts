56481c6c7aeddcbdaffb1d140573c8a5b37eac61
Evgenii Zaichkin <evgeniy.v.zaichkin@gmail.com>
2024-04-05 17:25:03 +0200

    Update version to 0.7.22

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.trainer.srb.rus"
         minSdk = 24
         targetSdk = 34
-        versionCode = 622
-        versionName = "0.6.22"
+        versionCode = 722
+        versionName = "0.7.22"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
