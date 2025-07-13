5440ad86397b5206cbb86e0e2e8104dd023d57c4
Evgenii Zaichkin <ezaichkin@onevizion.com>
2025-07-13 15:21:01 +0500

    Update the application version to 0.16.31 (build release with new translation API keys).

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.trainer.srb.rus"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1531
-        versionName = "0.15.31"
+        versionCode = 1631
+        versionName = "0.16.31"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
